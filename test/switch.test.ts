de3cf64a7bad4d17d7f559bb2bac94f5bfea2c53
Yuliana <yulilopezmar23@gmail.com>
2022-04-06 22:02:46 -0500

    pruebas

---
+++ test/switch.test.ts
@@ -0,0 +1,25 @@
+import {translateUnac} from "../src/switch-translate";
+
+test("traducir Hi a Hola", () =>{
+    //probar
+    expect(translateUnac('hi')).toMatch(/hola/);
+});
+
+test("traducir bye a adios", () =>{
+    //probar
+    expect(translateUnac('bye')).toMatch(/adios/);
+});
+test("traducir moon a luna", () =>{
+    //probar
+    expect(translateUnac('moon')).toMatch(/luna/);
+});
+test("traducir moon a luna", () =>{
+    //probar
+    expect(translateUnac('Moon')).toMatch(/default/);
+});
+
+// test malos 
+test("traducir moon a luna", () =>{
+    //probar
+    expect(translateUnac('moon')).toMatch(/Luna/);
+});
