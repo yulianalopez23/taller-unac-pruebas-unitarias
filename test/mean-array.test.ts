0cda2d764e856f0d816f058d23ad44a6b2981a36
stephania25 <stephania.osoriog@unac.edu.co>
2022-04-06 22:50:25 -0500

    prueba mean error

--- test/mean-array.test.ts
+++ test/mean-array.test.ts
@@ -3,3 +3,8 @@ import {mean} from '../src/mean-array';
 test("al resultado de 4.5 (6+3)/2", () =>{
     expect(mean([6,3])).toBe(4.5);
     });
+
+    
+test("al resultado de 1.5 (5+5)/2", () =>{
+    expect(mean([5,5])).toBe(1.5);
+    });
